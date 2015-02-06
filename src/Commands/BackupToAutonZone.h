6f4aacbff8c8dfd56e2cc226add2917561be3992
Miles Aikens <snekiam@gmail.com>
2015-02-05 20:50:08 -0500

    Command based auton code (needs testing)

---
+++ src/Commands/BackupToAutonZone.h
@@ -0,0 +1,20 @@
+#ifndef BACKUPTOAUTONZONE_H
+#define BACKUPTOAUTONZONE_H
+
+
+#include "Commands/Subsystem.h"
+#include "../Robot.h"
+
+class BackupToAutonZone: public Command {
+	double startTime;
+	double duration;
+public:
+	BackupToAutonZone();
+	virtual void Initialize();
+	virtual void Execute();
+	virtual bool IsFinished();
+	virtual void End();
+	virtual void Interrupted();
+};
+
+#endif
---
+++ src/Commands/GatherCans.cpp
@@ -0,0 +1,12 @@
+#include "GatherCans.h"
+#include "DriveForward.h"
+#include "AlignWithStep.h"
+#include "RotateWings.h"
+#include "BackupToAutonZone.h"
+
+GatherCans::GatherCans() {
+	AddSequential(new DriveForward());
+	AddSequential(new AlignWithStep());
+	AddSequential(new RotateWings());
+	AddSequential(new BackupToAutonZone());
+}
