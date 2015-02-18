ee64ec9978d5725c6ed935d10d5657b0428700ba
Miles Aikens <snekiam@gmail.com>
2015-02-17 19:14:48 -0500

    Added camera, not bad commands removed servo crap.

--- Simulate/src/Commands/subdir.mk
+++ Simulate/src/Commands/subdir.mk
@@ -4,8 +4,10 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
+../src/Commands/APusH.cpp \
 ../src/Commands/AlignWithStep.cpp \
 ../src/Commands/AutonCalibration.cpp \
+../src/Commands/AutonJerky.cpp \
 ../src/Commands/AutonomousCommand.cpp \
 ../src/Commands/BackupToAutonZone.cpp \
 ../src/Commands/DriveForward.cpp \
@@ -15,14 +17,20 @@ CPP_SRCS += \
 ../src/Commands/LowerOneTote.cpp \
 ../src/Commands/RaiseOneTote.cpp \
 ../src/Commands/RaiseWings.cpp \
+../src/Commands/ResetLift.cpp \
 ../src/Commands/RetractWings.cpp \
 ../src/Commands/RotateWings.cpp \
 ../src/Commands/RunLift.cpp \
+../src/Commands/ShiftToHighGear.cpp \
+../src/Commands/ShiftToLowGear.cpp \
+../src/Commands/UnAPusH.cpp \
 ../src/Commands/WingsTeleop.cpp 
 
 OBJS += \
+./src/Commands/APusH.o \
 ./src/Commands/AlignWithStep.o \
 ./src/Commands/AutonCalibration.o \
+./src/Commands/AutonJerky.o \
 ./src/Commands/AutonomousCommand.o \
 ./src/Commands/BackupToAutonZone.o \
 ./src/Commands/DriveForward.o \
@@ -32,14 +40,20 @@ OBJS += \
 ./src/Commands/LowerOneTote.o \
 ./src/Commands/RaiseOneTote.o \
 ./src/Commands/RaiseWings.o \
+./src/Commands/ResetLift.o \
 ./src/Commands/RetractWings.o \
 ./src/Commands/RotateWings.o \
 ./src/Commands/RunLift.o \
+./src/Commands/ShiftToHighGear.o \
+./src/Commands/ShiftToLowGear.o \
+./src/Commands/UnAPusH.o \
 ./src/Commands/WingsTeleop.o 
 
 CPP_DEPS += \
+./src/Commands/APusH.d \
 ./src/Commands/AlignWithStep.d \
 ./src/Commands/AutonCalibration.d \
+./src/Commands/AutonJerky.d \
 ./src/Commands/AutonomousCommand.d \
 ./src/Commands/BackupToAutonZone.d \
 ./src/Commands/DriveForward.d \
@@ -49,9 +63,13 @@ CPP_DEPS += \
 ./src/Commands/LowerOneTote.d \
 ./src/Commands/RaiseOneTote.d \
 ./src/Commands/RaiseWings.d \
+./src/Commands/ResetLift.d \
 ./src/Commands/RetractWings.d \
 ./src/Commands/RotateWings.d \
 ./src/Commands/RunLift.d \
+./src/Commands/ShiftToHighGear.d \
+./src/Commands/ShiftToLowGear.d \
+./src/Commands/UnAPusH.d \
 ./src/Commands/WingsTeleop.d 
 
 
